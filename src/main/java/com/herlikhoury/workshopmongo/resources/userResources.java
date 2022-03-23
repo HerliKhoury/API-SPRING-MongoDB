a9b395dfa5edb4e5888b079d569e14096b3d9678
Herli Khoury <herli.kh@gmail.com>
2022-03-23 19:44:58 -0300

    Lista_de_usuários

--- src/main/java/com/herlikhoury/workshopmongo/resources/userResources.java
+++ src/main/java/com/herlikhoury/workshopmongo/resources/userResources.java
@@ -1,11 +1,25 @@
 package com.herlikhoury.workshopmongo.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.herlikhoury.workshopmongo.domain.user;
 
 @RestController //necessário para a classer ser um recurso Rest
 @RequestMapping(value="/users")//end point
 
 public class userResources {
+	
+	public List<user> findAll(){
+		user gandalf = new user("1", "Gandalf the Gray", "gray@wizard.com");
+		user thorin = new user("2", "Thorin Oaken Shield", "king@mountain.com");
+		List<user> lista= new ArrayList<>();
+		lista.addAll(Arrays.asList(gandalf, thorin));
+		
+		return lista;
+		
+	}
 
 }
