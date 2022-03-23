f7f8e47680d56d7da4b1931440fb369fa03beb2b
Herli Khoury <herli.kh@gmail.com>
2022-03-23 19:33:14 -0300

    Resources_endpoint

--- src/main/java/com/herlikhoury/workshopmongo/resources/userResources.java
+++ src/main/java/com/herlikhoury/workshopmongo/resources/userResources.java
@@ -1,5 +1,11 @@
 package com.herlikhoury.workshopmongo.resources;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //necessário para a classer ser um recurso Rest
+@RequestMapping(value="/users")//end point
+
 public class userResources {
 
 }
