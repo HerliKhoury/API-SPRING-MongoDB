88b7535191c924d5e292ff88161077cd0057cf52
Herli Khoury <herli.kh@gmail.com>
2022-03-23 21:21:00 -0300

    Método que retorna lista de users

--- src/main/java/com/herlikhoury/workshopmongo/services/userServ.java
+++ src/main/java/com/herlikhoury/workshopmongo/services/userServ.java
@@ -1,8 +1,21 @@
 package com.herlikhoury.workshopmongo.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.herlikhoury.workshopmongo.domain.user;
+import com.herlikhoury.workshopmongo.repository.userRepository;
 
 @Service
 public class userServ {
+	
+	@Autowired
+	private userRepository repo;
+	
+	public List<user> findAll(){
+		return repo.findAll();
+	}
 
 }
