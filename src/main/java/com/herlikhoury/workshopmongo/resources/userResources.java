bf90dd58a5625dda1456853e79186ee7d2f31b13
Herli Khoury <herli.kh@gmail.com>
2022-03-23 21:43:34 -0300

    Busca users do MongoDB

--- src/main/java/com/herlikhoury/workshopmongo/resources/userResources.java
+++ src/main/java/com/herlikhoury/workshopmongo/resources/userResources.java
@@ -4,28 +4,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.herlikhoury.workshopmongo.domain.user;
+import com.herlikhoury.workshopmongo.services.userServ;
 
 @RestController //necessário para a classer ser um recurso Rest
 @RequestMapping(value="/users")//end point
 
 public class userResources {
 	
+	@Autowired
+	private userServ service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<user>> findAll(){
-		
-		user gandalf = new user("1", "Gandalf the Gray", "gray@wizard.com");
-		user thorin = new user("2", "Thorin Oaken Shield", "king@mountain.com");
-		
-		List<user> lista= new ArrayList<>();
-		lista.addAll(Arrays.asList(gandalf, thorin));
-		
-		return ResponseEntity.ok().body(lista);
+
+	List<user> lista= service.findAll();
+	
+	return ResponseEntity.ok().body(lista);
 		
 	}
 
