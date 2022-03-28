d80a1e620ffc6111b58bccf32a866f2b27b1c977
Herli Khoury <herli.kh@gmail.com>
2022-03-28 19:50:53 -0300

    findById_userServ

--- src/main/java/com/herlikhoury/workshopmongo/services/userServ.java
+++ src/main/java/com/herlikhoury/workshopmongo/services/userServ.java
@@ -1,12 +1,14 @@
 package com.herlikhoury.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.herlikhoury.workshopmongo.domain.user;
 import com.herlikhoury.workshopmongo.repository.userRepository;
+import com.herlikhoury.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class userServ {
@@ -16,6 +18,11 @@ public class userServ {
 	
 	public List<user> findAll(){
 		return repo.findAll();
+	}
+	
+	public user findById(String id) {
+		Optional<user> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
