43b9bce0c28a56698424b0a55ba1c09f99c803e5
Herli Khoury <herli.kh@gmail.com>
2022-03-28 19:34:35 -0300

    Classe ObjectNotFoundException

---
+++ src/main/java/com/herlikhoury/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.herlikhoury.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String mensagem) {
+		super(mensagem);
+	}
+	
+
+}
