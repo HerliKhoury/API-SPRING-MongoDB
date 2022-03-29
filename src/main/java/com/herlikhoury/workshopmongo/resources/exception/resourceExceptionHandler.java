095ebb318121728e58675fdfafe15bd48341ed39
Herli Khoury <herli.kh@gmail.com>
2022-03-29 09:11:41 -0300

    Tratamento de erro 404

--- src/main/java/com/herlikhoury/workshopmongo/resources/exception/resourceExceptionHandler.java
+++ src/main/java/com/herlikhoury/workshopmongo/resources/exception/resourceExceptionHandler.java
@@ -1,5 +1,32 @@
 package com.herlikhoury.workshopmongo.resources.exception;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.herlikhoury.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice //Indica que a classe irá tratar possíveis erros de requisição
 public class resourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)//Necessário pra indicar quando houver esta exceção, fazer o tratamento abaixo
+	public ResponseEntity<standardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;//Armazena o erro 404
+		
+		standardError err = new standardError(
+				System.currentTimeMillis(), 
+				status.value(), 
+				"Herói não encontrado", 
+				e.getMessage(), 
+				request.getRequestURI()
+		);
+		
+		return ResponseEntity.status(status).body(err);
+		
+	}
 
 }
