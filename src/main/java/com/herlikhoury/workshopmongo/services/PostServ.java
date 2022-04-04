91186c583f68718fe86e4713502710d5c2a7ce0e
Herli Khoury <herli.kh@gmail.com>
2022-04-04 18:04:27 -0300

    PostServ_findById

---
+++ src/main/java/com/herlikhoury/workshopmongo/services/PostServ.java
@@ -0,0 +1,25 @@
+package com.herlikhoury.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.herlikhoury.workshopmongo.domain.Post;
+import com.herlikhoury.workshopmongo.repository.PostRepository;
+import com.herlikhoury.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostServ {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Post findById(String id) {
+	    Optional<Post> user = repo.findById(id);
+	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+	
+}
