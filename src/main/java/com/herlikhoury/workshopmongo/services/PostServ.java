d40c96ad145cef17139b46a82ba894c28f9b3bd0
Herli Khoury <herli.kh@gmail.com>
2022-04-05 12:40:31 -0300

    Serviço findByTitle_PostServ

--- src/main/java/com/herlikhoury/workshopmongo/services/PostServ.java
+++ src/main/java/com/herlikhoury/workshopmongo/services/PostServ.java
@@ -1,5 +1,6 @@
 package com.herlikhoury.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class PostServ {
 	public Post findById(String id) {
 	    Optional<Post> user = repo.findById(id);
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+	public List<Post> findByTitle (String text){
+		return repo.findByTitleContaining(text);
 	}
 	
 	
